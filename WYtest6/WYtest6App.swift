5185103c478f1edda53bb44e55c12ceffd6b739e
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-03-02 22:38:05 +0800

    Initial Commit

---
+++ WYtest6/WYtest6App.swift
@@ -0,0 +1,17 @@
+//
+//  WYtest6App.swift
+//  WYtest6
+//
+//  Created by test on 2024/3/2.
+//
+
+import SwiftUI
+
+@main
+struct WYtest6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
