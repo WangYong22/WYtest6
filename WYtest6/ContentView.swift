65a6479ecab33d19dcccabb8d111f85ae50d6b0b
WangYong22 <102638006+WangYong22@users.noreply.github.com>
2024-03-04 23:39:43 +0800

    hello2

--- WYtest6/ContentView.swift
+++ WYtest6/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello1, world!")
+            Text("Hello2, world!")
         }
         .padding()
     }
